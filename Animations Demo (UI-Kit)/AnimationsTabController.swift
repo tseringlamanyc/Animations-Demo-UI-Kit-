ef267b0ec12b55f1c6f74724ef71122dd103cce6
Tsering Lama <tseringlama@pursuit.org>
2020-01-31 10:53:17 -0500

    gave tab bar names and images

--- Animations Demo (UI-Kit)/AnimationsTabController.swift
+++ Animations Demo (UI-Kit)/AnimationsTabController.swift
@@ -10,12 +10,22 @@ import UIKit
 
 class AnimationsTabController: UITabBarController {
     
+    private lazy var sampleAnimation: SampleAnimationVC = {
+       let vc = SampleAnimationVC()
+        vc.tabBarItem = UITabBarItem(title: "Sample Animation", image: UIImage(systemName: "1.circle"), tag: 0)
+        return vc
+    }()
     
-
+    private lazy var constraintAnimation: ConstraintAnimationVC = {
+        let vc = ConstraintAnimationVC()
+        vc.tabBarItem = UITabBarItem(title: "Constraints Animation", image: UIImage(systemName: "2.circle"), tag: 1)
+        return vc
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         // set VC for the tab bar (2 tabs)
-        viewControllers = [SampleAnimationVC(), ConstraintAnimationVC()]
+        viewControllers = [sampleAnimation, constraintAnimation]
     }
 }
