40901d622abe1cc9bac8dcac9210dc8db4284177
Tsering Lama <tseringlama@pursuit.org>
2020-02-04 12:41:05 -0500

    added animations using UIViewAnimation

--- Animations Demo (UI-Kit)/VC/PropertyAnimatorVC.swift
+++ Animations Demo (UI-Kit)/VC/PropertyAnimatorVC.swift
@@ -9,22 +9,27 @@
 import UIKit
 
 class PropertyAnimatorVC: UIViewController {
-
+    
+    @IBOutlet weak var yasuo: UIImageView!
+    @IBOutlet weak var slider: UISlider!
+    
+    // instance of UIViewPropertyAnimator
+    
+    private var animator: UIViewPropertyAnimator!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        // instantitate the animator property
+        animator = UIViewPropertyAnimator(duration: 2.0, curve: .easeInOut, animations: {
+            // code for animation
+            self.yasuo.transform = CGAffineTransform(scaleX: 2.0, y: 2.0)
+        })
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    
+    @IBAction func sliderChanged(_ sender: UISlider) {
+        animator.fractionComplete = CGFloat(sender.value)
     }
-    */
-
+    
 }
